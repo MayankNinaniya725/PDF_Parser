4766ac085f7b8099192d4a5ca83ba86a489573fa
MayankNinaniya725 <mninaniya499@gmail.com>
2025-09-22 16:12:38 +0530

    Apply .gitignore rules

--- extractor/templatetags/urllib_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import urllib.parse
-
-register = template.Library()
-
-@register.filter
-def urlencode(value):
-    """Encodes a value for use in a URL."""
-    return urllib.parse.quote(str(value))
--- extractor/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .auth import *
-from .core import *
-from .downloads import download_all_pdfs_package, download_package_api
